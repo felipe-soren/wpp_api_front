a0b9a3940f53f622180c7e59e9f3268321d2ebae
Felipe Novaes <felipe.soren@gmail.com>
2019-07-13 22:07:22 -0300

    initial commit

---
+++ js/actions.js
@@ -0,0 +1,11 @@
+function sendMessage(){ 
+   var message = document.getElementById("message");
+   var phone = document.getElementById("phone");;
+   
+   
+   
+   var xmlHttp = new XMLHttpRequest();
+   xmlHttp.open( "POST", `https://cors-anywhere.herokuapp.com/https://6fec724b.ngrok.io/sendmessage?tel=55${phone.value}&message=${message.value}`, false ); // false for synchronous request
+   xmlHttp.send();
+   alert(`Mensagem Enviada (A mensagem pode levar até 15 segundos para ser entregue ^^)`)
+}
