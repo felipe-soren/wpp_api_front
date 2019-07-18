efaf3a949864c3c132577b19dba8a0d8b9842b24
felipe-soren <30413876+felipe-soren@users.noreply.github.com>
2019-07-18 14:56:29 -0300

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -5,7 +5,7 @@ function sendMessage(){
    
    
    var xmlHttp = new XMLHttpRequest();
-   xmlHttp.open( "POST", `https://cors-anywhere.herokuapp.com/http://034fd1ac.ngrok.io/sendmessage?tel=55${phone.value}&message=${message.value}`, false ); // false for synchronous request
+   xmlHttp.open( "POST", `https://cors-anywhere.herokuapp.com/http://0ceae7d7.ngrok.io/sendmessage?tel=55${phone.value}&message=${message.value}`, false ); // false for synchronous request
    xmlHttp.send();
    alert(`Mensagem Enviada (A mensagem pode levar até 15 segundos para ser entregue ^^)`)
 }
